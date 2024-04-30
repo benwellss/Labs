c4cd87ea5cb738dc9128a42fda3a08cb1de94eaa
benwellss <146930771+benwellss@users.noreply.github.com>
2024-04-30 09:44:22 +0100

    Create test_calculator.py

---
+++ Testing/test_calculator.py
@@ -0,0 +1,28 @@
+import unittest
+from calculator import Calculator
+
+
+class TestCalculator(unittest.TestCase):
+
+    def test_addition(self):
+        calc = Calculator()
+        self.assertEqual(calc.add(3, 2), 5)
+        self.assertEqual(calc.add(-1, -1), -2)
+        self.assertEqual(calc.add(0, 0), 0)
+        self.assertEqual(calc.add(1000, 2000), 3000)
+
+    def test_subtraction(self):
+        calc = Calculator()
+        self.assertEqual(calc.subtract(5, 2), 3)
+
+    @unittest.skip("skipping...")
+    def test_skip(self):
+        pass
+
+    @unittest.skipIf(1 == 1, "skipping also")
+    def test_skip_if(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
